45fe75027e399272bf28a24a9b86c70d03275ddf
mjaydenkim <mjaydenkim@gmail.com>
2019-02-10 08:33:28 -0800

    Update website-config.ts

--- src/website-config.ts
+++ src/website-config.ts
@@ -29,8 +29,8 @@ export interface WebsiteConfig {
 }
 
 const config: WebsiteConfig = {
-  title: 'Ghost',
-  description: 'The professional publishing platform',
+  title: 'Osmium',
+  description: 'the 76th element',
   coverImage: 'img/blog-cover.jpg',
   logo: 'img/ghost-logo.png',
   lang: 'en',
